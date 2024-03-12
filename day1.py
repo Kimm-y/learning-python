6b9831305544a7f46048acf59df87c1dfce75b73
Kimm-y <kimmiemumbi789@gmail.com>
2024-03-12 11:13:43 +0300

    python day 1

---
+++ day1.py
@@ -0,0 +1,28 @@
+#CREATING VARIABLES
+#'const' ,'let', 'var' aren't used
+
+#RULES
+#You can use any character NOT A NUMBER, # , !
+#SNAKE CASE IS USED
+
+house_fly= 10
+name = 'Dan' 
+
+#TO EXECUTE
+print(name)
+
+#DATATYPES
+
+#Primitive
+
+#STRING- either use '' or ""
+#NUMBER - interger, float(decimals)
+#BOOLEAN- true or false
+#NONE-> NONE (equivalent of null in javascript)
+
+#Non-primitive
+
+#LIST
+#TUPLE
+#SET
+#DICTIONARY- uses curly brackets
